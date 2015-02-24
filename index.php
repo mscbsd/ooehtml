35760166b120cf9fc56048a5efb0d4eaa2fe3e36
mscbsd <mscbsd@gmail.com>
2015-02-23 23:59:31 -0300

    upload
    first upload

---
+++ index.php
@@ -0,0 +1,64 @@
+﻿<?php
+require_once('ooeHtml.class.php');
+
+$fileTemplate='ooeHtml.html5.html';
+
+$Objeto = new ooeHtml; // cria o objeto template
+
+$Objeto->setHtmlContent($fileTemplate); // carrega o template html com as marcacoes
+
+$Objeto->setHtmlVar('AUTHOR', 'Marcelo Soares da Costa');
+$Objeto->setHtmlVar('PACKAGE', 'ooeHtml');
+
+$Objeto->setHtmlVar('ASIDEIPSUM', 'Collaboratively administrate empowered markets via plug-and-play networks. Dynamically procrastinate B2C users after installed base benefits. Dramatically visualize customer directed convergence without revolutionary ROI.
+
+Efficiently unleash cross-media information without cross-media value. Quickly maximize timely deliverables for real-time schemas. Dramatically maintain clicks-and-mortar solutions without functional solutions.
+
+Completely synergize resource sucking relationships via premier niche markets. Professionally cultivate one-to-one customer service with robust ideas. Dynamically innovate resource-leveling customer service for state of the art customer service.');
+
+$Objeto->setHtmlVar('BEERIPSUM', 'lauter tun kolsch: alcohol; hand pump rims. ipa sour/acidic. conditioning tank bung wort! keg lagering; length: hoppy racking gravity, pint glass. specific gravity chocolate malt cask conditioned ale aau ipa pitching grainy. beer, balthazar: beer oxidized racking oxidized additive noble hops hydrometer, craft beer, heat exchanger. barley cask conditioned ale bung beer pub pint glass. bunghole fermentation malt yeast. double bock/dopplebock brewing bittering hops anaerobic berliner weisse? lauter trappist chocolate malt ibu wit mash. carboy hoppy, keg pilsner becher, barleywine heat exchanger, ipa.');
+
+$Objeto->setHtmlVar('BIGTITLE', 'Titulo Grande');
+
+$Objeto->setHtmlVar('ITEM1', 'Menu item 1');
+$Objeto->setHtmlVar('ITEM2', 'Menu item 2');
+$Objeto->setHtmlVar('ITEM3', 'Menu item 3');
+
+$Objeto->setHtmlVar('ARTICLE1', 'Artigo com título header h1');
+$Objeto->setHtmlVar('ARTICLE2', 'Artigo com título section h2');
+$Objeto->setHtmlVar('ARTICLE3', 'Artigo com título section h2 e bloco');
+$Objeto->setHtmlVar('ARTICLE4', 'Artigo com título footer h3 e bloco');
+
+$Objeto->setHtmlVar('CONTENT1', 'Silvio Santos Ipsum ma você, topa ou não topamm. Patríciaaammmm... Luiz Ricardouaaammmmmm. Vem pra lá, mah você vai pra cá. Agora vai, agora vem pra láamm. Qual é a musicamm? O arriscam tuduam, valendo um milhão de reaisuam. Ma quem quer dinheiroam? Patríciaaammmm... Luiz Ricardouaaammmmmm. Um, dois três, quatro, PIM, entendeuam? Boca sujuam... sem vergonhuamm. Boca sujuam... sem vergonhuamm. É namoro ou amizadeemm?');
+$Objeto->setHtmlVar('CONTENT2', 'Eiiitaaa Mainhaaa!! Esse Lorem ipsum é só na sacanageeem!! E que abundância meu irmão viuu!! Assim você vai matar o papai. Só digo uma coisa, Domingo ela não vai! Danadaa!! Vem minha odalisca, agora faz essa cobra coral subir!!! Pau que nasce torto, Nunca se endireita. Tchannn!! Tchannn!! Tu du du pááá! Eu gostchu muitchu, heinn! danadinha! Mainhaa! Agora use meu lorem ipsum ordinária!!! Olha o quibeee! rema, rema, ordinária!.');
+
+@$block_array[]=array('auhor'=>'Silvio Santos','content'=>'Quem quer dinheiro !?','url'=>'http://silviosantosipsum.com/');
+$block_array[]=array('auhor'=>'Cumpade Wash','content'=>'Eitá mainha !','url'=>'http://compadreipsum.com.br/');
+$block_array[]=array('auhor'=>'Mussum','content'=>'Cacildis','url'=>'http://mussumipsum.com/');
+$block_array[]=array('auhor'=>'Dilma','content'=>'Dilmês Ipsum','url'=>'http://dilmesipsum.com.br/');
+
+$Objeto->setHtmlBlock('CONTENT3');
+
+foreach ($block_array as $value)
+{
+	$Objeto->setHtmlBlockVar('CONTENT3','AUTHORIPSUM',$value['auhor']);
+	$Objeto->setHtmlBlockVar('CONTENT3','FRASEIPSUM',$value['content']);
+	$Objeto->setHtmlBlockVar('CONTENT3','URLIPSUM',$value['url']);
+	$Objeto->setHtmlBlockVar('CONTENT3','STATICIPSUM','estático'); // teste estatico
+}
+
+@$block_array2[]=array('CONTENT40'=>'alguma coisa 1','CONTENT41'=>'teste 1');
+$block_array2[]=array('CONTENT40'=>'alguma coisa 2','CONTENT41'=>'teste 2');
+$block_array2[]=array('CONTENT40'=>'alguma coisa 3','CONTENT41'=>'teste 3');
+$block_array2[]=array('CONTENT40'=>'alguma coisa 4','CONTENT41'=>'teste 4');
+
+$Objeto->setHtmlBlock('CONTENT4');
+
+foreach ($block_array2 as $value2)
+{
+	$Objeto->setHtmlBlockVar('CONTENT4','CONTENT40',$value2['CONTENT40']);
+	$Objeto->setHtmlBlockVar('CONTENT4','CONTENT41',$value2['CONTENT41']);
+}
+
+$Objeto->printHtmlScreen();
+?>
---
+++ ooeHtml.class.php
@@ -0,0 +1,193 @@
+<?php
+/**
+* Classe que retorna html a partir de um template php com marcações especiais
+* @author Marcelo Soares da Costa
+* @email mscbsd at yahoo dot com
+* @copyright Marcelo Soares da Costa © 2015.
+* @license FreeBSD http://www.freebsd.org/copyright/freebsd-license.html
+* @version 1.0
+* @access public
+* @package OOE
+* @data 2015-03-01
+*/
+################################################################################
+class ooeHtml{
+
+	private $blocKeys=array();
+  private $blockValues=array();
+	private $data=array();
+	private $result=null;
+	private $content=null;
+	private $etc=null;
+
+	function setHtmlContent($fileTemplate)
+	{
+		$this->content=file_get_contents($fileTemplate);
+	}
+
+	private function getHtmlContent()
+	{
+  // replace multiple new lines with a single newline
+  //  $output = preg_replace(array('/\s+$/Sm', '/\n+/S'), "\n", $output);
+    return preg_replace(array('/\s+$/Sm', '/\n+/S'), "\n", $this->content);//$this->content;
+	}
+
+	function setHtmlVar($name, $value)
+	{
+		$this->etc['{{'.$name.'}}'] = $value;
+	}
+
+	private function getHtmlValues()
+	{
+		return $this->etc;
+	}
+
+	function getHtmlVar($name)
+	{
+		if (array_key_exists($name, $this->data)) {
+			return $this->data['{{'.$name.'}}'];
+		}elseif(method_exists($this, $name)){
+			return $this->$name();
+		} else {
+			throw new Exception("Property $name does not exist");
+		}
+	}
+
+	public function setHtmlBlock($block)
+	{
+		$this->BLOCKS[]=strtoupper($block);
+	}
+
+	private function getHtmlBlocks()
+	{
+		return $this->BLOCKS;
+	}
+
+	public function setHtmlBlockVar($block,$name,$value)
+	{
+		$this->blocKeys[strtoupper($block)][]='{{'.$name.'}}';
+		$this->blockValues[strtoupper($block)][]=$value;
+	}
+
+
+	private function parseHtmlContent($BLOCK,$arrayData)
+	{
+
+		$reg = "/<!--\s*#START\sBLOCK\s:\s+$BLOCK\s+-->\s*(\s*.*?\s*)<!--\s+#END\sBLOCK\s:\s+$BLOCK\s*-->\s*/sm";
+
+		$code = preg_replace("#(.*)<!--\s\#START BLOCK\s:\s".$BLOCK."\s-->(.*?)<!--\s\#END BLOCK\s:\s".$BLOCK."\s-->(.*)#is", '$2', $this->content);
+
+		foreach ($arrayData as $key=>$line)
+		{
+			$this->result.= preg_replace(array_keys($line),array_values($line), $code);
+		}
+
+		$this->content = preg_replace($reg,$this->result,self::getHtmlContent());
+
+		unset($arrayData);
+		$this->result=null;
+	}
+
+	public function parseHtmlBlocks()
+	{
+
+		foreach (self::getHtmlBlocks() as $BLOCK)
+		{
+
+			$keys=array_unique(array_values($this->blocKeys[$BLOCK]));
+
+			$values= array_values($this->blockValues[$BLOCK]);
+
+			$combinearray=self::array_combine_multidimensional($keys,$values);
+
+			unset($this->blockValues[$BLOCK]);
+			unset($this->blocKeys[$BLOCK]);
+
+			self::parseHtmlContent($BLOCK,$combinearray);
+
+			unset($combinearray);
+		}
+		return self::getHtmlContent();
+	}
+
+	public function printHtmlScreen()
+	{
+		$print = preg_replace(array_keys(self::getHtmlValues()), array_values(self::getHtmlValues()), self::parseHtmlBlocks());
+		ob_start();
+		echo $print;
+		ob_end_flush();
+	}
+
+	// from php manual array_combine at bradentkeith at dot dontspam dot gmail dot com
+	private function array_combine_multidimensional($arr1, $arr2) {
+		$count1 = count($arr1);
+		$count2 = count($arr2);
+		$numofloops = $count2/$count1;
+
+		$i = 0;
+		while($i < $numofloops){
+			$arr3 = array_slice($arr2, $count1*$i, $count1);
+			$arr4[] = array_combine($arr1,$arr3);
+			$i++;
+		}
+
+		return $arr4;
+	}
+
+	// from php preg_replace manual ( hvishnu999 at gmail dot com)
+	private function seoHtmlFriend($str)
+	{
+
+		$seoname = preg_replace('/\%/',' percentage',$str);
+		$seoname = preg_replace('/\@/',' at ',$seoname);
+		$seoname = preg_replace('/\&/',' and ',$seoname);
+		$seoname = preg_replace('/\s[\s]+/','-',$seoname);    // Strip off multiple spaces
+		$seoname = preg_replace('/[\s\W]+/','-',$seoname);    // Strip off spaces and non-alpha-numeric
+		$seoname = preg_replace('/^[\-]+/','',$seoname); // Strip off the starting hyphens
+		$seoname = preg_replace('/[\-]+$/','',$seoname); // // Strip off the ending hyphens
+		$seoname = strtolower($seoname);
+
+		return $seoname;
+	}
+
+//  from php preg_replace manual (anyvie at devlibre dot fr )
+	function htmlHead($url)
+	{
+		/*
+		$head can have the desired content, or be empty, depends on the length of $data.
+
+		For this application, just add :
+		$data = substr($data, 0, 4096);
+		before using preg_replace, and it will work fine.
+		*/
+		// We get all the data into $data
+		$data = file_get_contents($url);
+		// We just want to keep the content of <head>
+		$head = preg_replace("#(.*)<head>(.*?)</head>(.*)#is", '$2', $data);
+
+		return $head;
+	}
+
+	// from php preg_replace manual (erik dot stetina at gmail dot com)
+	function removeHtmlComments(&$string)
+	{
+		$string = preg_replace("%(#|;|(//)).*%","",$string);
+		$string = preg_replace("%/\*(?:(?!\*/).)*\*/%s","",$string); // google for negative lookahead
+		return $string;
+	}
+
+	private function array_flatten($a){ //flattens multi-dim arrays (distroys keys)
+		$ab = array(); if(!is_array($a)) return $ab;
+		foreach($a as $value){
+			if(is_array($value)){
+				$ab = array_merge($ab,array_flatten($value));
+			}else{
+				array_push($ab,$value);
+			}
+		}
+		return $ab;
+	}
+
+// end
+}
+?>
